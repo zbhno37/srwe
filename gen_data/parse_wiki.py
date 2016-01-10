700e332977e0a6a6a76c0f952e8779217330668e
zbhno37 <zbhno37@gmail.com>
2016-01-10 10:53:55 +0800

    wiki word count

--- gen_data/parse_wiki.py
+++ gen_data/parse_wiki.py
@@ -1,6 +1,7 @@
 #coding=utf-8
 import logging
 from gensim.corpora import WikiCorpus
+from collections import defaultdict
 logging.basicConfig(format='%(asctime)s\t%(message)s', level=logging.INFO)
 
 def parse_wiki(filename):
@@ -16,8 +17,26 @@ def parse_wiki(filename):
     fout.close()
     logging.info('Finish %d' % count)
 
+def word_freq(filename):
+    fout = file('../../paper/data/wiki/wiki_corpus.vocab', 'w')
+    freq = defaultdict(lambda: 0)
+    count = 0
+    with open(filename) as fin:
+        for line in fin:
+            words = line.strip().split(' ')
+            for word in words:
+                freq[word] += 1
+            if count % 10000 == 0:
+                logging.info(count)
+            count += 1
+    sorted_words = sorted(freq.items(), key=lambda x:x[1], reverse=True)
+    for item in sorted_words:
+        fout.write('%s\t%s\n' % (item[0], item[1]))
+    fout.close()
+
 def main():
-    parse_wiki('../../paper/data/wiki/enwiki-latest-pages-articles.xml.bz2')
+    #parse_wiki('../../paper/data/wiki/enwiki-latest-pages-articles.xml.bz2')
+    word_freq('../../paper/data/wiki/wiki_corpus')
 
 if __name__ == '__main__':
     main()
